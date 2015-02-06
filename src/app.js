86f3c25d322952de88459e6d5d8f3dd1bed74ee6
DamoFlamo <DamoFlamo@users.noreply.github.com>
2015-02-06 16:43:55 +1100

    V1
    First Version

---
+++ src/app.js
@@ -0,0 +1,80 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+//var Vector2 = require('vector2');
+
+var mainTableView = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'P1: Geography',
+        subtitle: 'R213'
+      }, {
+        title: 'P2: IT',
+        subtitle: 'MMC'
+      }, {
+        title: 'P3: Chinese',
+        subtitle: 'S102'
+      }, {
+        title: 'P4: English',
+        subtitle: 'R102'
+      }, {
+        title: 'P5: History',
+        subtitle: 'R202'
+      }]
+    }]
+  });
+mainTableView.show();
+
+/*var main = new UI.Card({
+  title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
+  subtitle: 'Hello World!',
+  body: 'Press any button.'
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Pebble.js',
+        icon: 'images/menu_icon.png',
+        subtitle: 'Can do Menus'
+      }, {
+        title: 'Second Item',
+        subtitle: 'Subtitle Text'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window();
+  var textfield = new UI.Text({
+    position: new Vector2(0, 50),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'Text Anywhere!',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('A Card');
+  card.subtitle('Is a Window');
+  card.body('The simplest window type in Pebble.js.');
+  card.show();
+});*/
